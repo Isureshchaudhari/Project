f52d6ba95eb3e3092ff3fe24fb5d37986f2d9011
Suresh Chaudhari <91820449+Isureshchaudhari@users.noreply.github.com>
2022-03-30 15:41:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log()
+$(document).ready(function(){
+
+    // top sale owl carousel
+    $("#top-sale .owl-carousel").owlCarousel({
+      loop: true,
+      nav: true,
+      dots: false,
+      responsive : {
+          0: {
+              items: 1
+          },
+          600: {
+              items: 3
+          },
+          1000 : {
+              items: 5
+          }
+      }
+  });
+  
+  });
