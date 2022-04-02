a3cc5d34eefa476755a0d10bdc5f2f1a510add78
Suresh Chaudhari <91820449+Isureshchaudhari@users.noreply.github.com>
2022-04-02 09:58:01 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -17,5 +17,18 @@ $(document).ready(function(){
           }
       }
   });
+
+   // isotope filter
+   var $grid = $(".grid").isotope({
+    itemSelector : '.grid-item',
+    layoutMode : 'fitRows'
+});
+
+// filter items on button click
+$(".button-group").on("click", "button", function(){
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue});
+})
+
   
   });
